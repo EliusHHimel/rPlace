ee93caa4bb97a1ff39a05397d28c12c32c4b4c12
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2023-07-22 01:34:13 +0600

    all config

--- main.ts
+++ main.ts
@@ -15,9 +15,8 @@ async function fetchAccessToken(username, password, clientId, clientSecret) {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
-        "authorization": `Basic ${
-          Buffer.from(`${clientId}:${clientSecret}`).toString("base64")
-        }`,
+        "authorization": `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString("base64")
+          }`,
       },
     },
   );
